d8c90e17e2c170d3ceed9076912eefaf02b3ac6d
ku13shov <kuleshov000@gmail.com>
2019-11-27 16:24:55 +0300

    new functional for mobile version

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// fixed header
+
 $(function() {
   let header = $("#header");
   let intro = $("#intro");
@@ -16,11 +18,30 @@ $(function() {
 
   });
 
-  
+  // smooth scroll
 
+  $("[data-scroll]").on("click", function(event){
+    event.preventDefault();
 
-  
+    let elementId = $(this).data('scroll');
+    let elementOffset = $(elementId).offset().top;
 
+    nav.removeClass("show");
 
+    $("html, body").animate({
+      scrollTop: elementOffset - 70
+    }, 800);
+  });
+
+  // navToggle
+
+  let nav = $("#nav");
+  let navToggle = $("#navToggle");
+
+  navToggle.on("click", function(event){
+    event.preventDefault();
+
+    nav.toggleClass("show");
+  });
 
 });
